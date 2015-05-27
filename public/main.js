9f6a336539602bca00a7e7e8f6136deb8624d82e
James Cummings <jamesjcummings@gmail.com>
2015-05-26 18:59:06 -0600

    end of day one

--- public/main.js
+++ public/main.js
@@ -1,1 +1,20 @@
 var politicalApp = angular.module('politicalApp', ['ngResource', 'ngRoute']);
+
+politicalApp.config(function($routeProvider){
+  $routeProvider
+    .when('/d', {
+      templateUrl: '/d',
+      controller: ''
+    })
+    .when('/state/:id', {
+      templateUrl: '/templates/state',
+      controller: 'stateController'
+    });
+});
+
+politicalApp.controller('stateController', function($scope, newsItems, $routeParams){
+  console.log($routeParams.id);
+  var postId = $routeParams.id;
+
+  $scope.news = newsItems.model.get({_id: postId});
+});
